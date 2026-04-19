e6f0cae61554d3f558c001ea1e9c95de3bde2f13
bukhari169 <amjad69uk@gmail.com>
2026-04-19 15:57:59 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,39 @@
 "use client";
 
+import { auth, provider } from "./firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
+import { useState } from "react";
+
 export default function Page() {
+  const [user, setUser] = useState(null);
+
+  const login = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+    } catch (error) {
+      console.log(error);
+      alert("Login failed");
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+    setUser(null);
+  };
+
   return (
     <main style={{ padding: 40 }}>
-      <h1>🚀 Login Test</h1>
-      <p>If you see this, deploy works.</p>
+      <h1>🚀 Amjad SaaS</h1>
+
+      {!user ? (
+        <button onClick={login}>Login with Google</button>
+      ) : (
+        <div>
+          <p>Welcome {user.displayName}</p>
+          <button onClick={logout}>Logout</button>
+        </div>
+      )}
     </main>
   );
 }
