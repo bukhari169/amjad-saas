0068f545406cf3e85133b1d3ec1a006a4acfad04
bukhari169 <amjad69uk@gmail.com>
2026-04-19 15:15:36 +0300

    Update firebase.js

--- app/app/firebase.js
+++ app/app/firebase.js
@@ -1,16 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "PUT_HERE",
-  authDomain: "PUT_HERE",
-  projectId: "amjad-saas",
-  storageBucket: "PUT_HERE",
-  messagingSenderId: "PUT_HERE",
-  appId: "PUT_HERE"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
